2ad6f08a5800020c85da866081450166f68675f3
motasjw <lucasm.lopes087@gmail.com>
2021-11-03 16:40:08 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button>Click</button>
+      <h1>Recipe Name</h1>
+
+      <h3>Ingredients</h3>
+      <ul>
+        <li>ingredient</li>
+      </ul>
+
+      <h3>Instructions</h3>
+      <ul>
+        <li>instruction</li>
+      </ul>
+
+      <img src="" alt="Food image" />
     </div>
   );
 }
