4ca334720390ae392d53943eb7461471b9ba3ee6
motasjw <lucasm.lopes087@gmail.com>
2021-11-08 19:18:01 -0300

    Feature: ingredients and measures

--- src/App.js
+++ src/App.js
@@ -15,25 +15,34 @@ function App() {
       console.error(error);
     } finally {
       setLoading(false);
+      getIngredients();
     }
   };
+
+  const getIngredients= () => {
+    var ingredientArray = [];
+    if (isLoading == false) {
+      for (var i = 1; i <= 20; i++) {
+        if ((data[0]['strIngredient' + i]) != "" && data[0]['strIngredient' + i] != null) {
+          let ingredient = String(data[0]['strMeasure' + i].trimEnd() + " " + data[0]['strIngredient' + i]);
+          ingredientArray.push(<li key={i}>{ingredient}</li>);
+        }
+      }
+    }
+    return (ingredientArray);
+  }
 
   return (
     <>
     <button onClick={() => getMeals()}>Click</button> 
     {isLoading == false ? 
       <div className="container">
-        
         <h1>{data[0].strMeal}</h1>
         <h3>Ingredients</h3>
-        <ul>
-          <li>ingredient</li>
-        </ul>
+        <ul>{getIngredients()}</ul>
         <h3>Instructions</h3>
-        <ul>
-          <li>instruction</li>
-        </ul>
-        <img src={data[0].strMealThumb} alt="Food image" />
+        <p>Instruction</p>
+        <img src={data[0].strMealThumb} alt="Food image" width="300" />
       </div>
     : null}
     </>
